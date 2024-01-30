c9189a7ebbd590f31f2c0d9f4280ee0896285877
Luciano Favoroso <luciano.favoroso@alliander.com>
2024-01-30 11:49:29 +0100

    FDP-1815: formatting
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- application/src/main/resources/db/migration/V2__secret.sql
+++ application/src/main/resources/db/migration/V2__secret.sql
@@ -2,8 +2,7 @@
 --
 -- SPDX-License-Identifier: Apache-2.0
 -- No production data was set before this change so we can drop all existing data
-delete
-from pre_shared_key;
+delete from pre_shared_key;
 
 alter table pre_shared_key
     add column secret varchar(255) not null;
