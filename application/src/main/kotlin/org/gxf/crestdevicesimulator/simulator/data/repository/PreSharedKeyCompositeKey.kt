18145fab6bdd9d48d85da80ad3d0cc63fcf28ab3
Loes Immens <loes.immens@alliander.com>
2024-03-22 15:17:58 +0100

    FDP-1961: copyright headers
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdevicesimulator/simulator/data/repository/PreSharedKeyCompositeKey.kt
+++ application/src/main/kotlin/org/gxf/crestdevicesimulator/simulator/data/repository/PreSharedKeyCompositeKey.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.gxf.crestdevicesimulator.simulator.data.repository
 
 import java.io.Serializable
