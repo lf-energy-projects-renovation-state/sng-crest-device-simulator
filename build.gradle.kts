a74504f82eed9b3d26b55fa0308bdced206c0a16
Jasper Kamerling <jasper.kamerling@alliander.com>
2024-07-19 12:19:50 +0200

    FDP-2470: Update dependencies
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
-    id("org.springframework.boot") version "3.3.0" apply false
+    id("org.springframework.boot") version "3.3.2" apply false
     id("io.spring.dependency-management") version "1.1.5" apply false
     kotlin("jvm") version "2.0.0" apply false
     kotlin("plugin.spring") version "2.0.0" apply false
