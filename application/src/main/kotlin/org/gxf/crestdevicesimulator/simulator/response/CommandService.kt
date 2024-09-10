710e51fd04684f7aae546859d2da0f92e79f6a4f
Loes Immens <loes.immens@alliander.com>
2024-09-10 16:51:18 +0200

    FDP-2359: review comments
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdevicesimulator/simulator/response/CommandService.kt
+++ application/src/main/kotlin/org/gxf/crestdevicesimulator/simulator/response/CommandService.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.gxf.crestdevicesimulator.simulator.response
 
 import org.springframework.stereotype.Service
